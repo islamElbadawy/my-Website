581f1704f1e9748f16b729af6d58149e8e803169
islamElbadawy <islam.201211@gmail.com>
2020-04-21 08:11:16 +0200

    uploading files

---
+++ includes/js/script.js
@@ -0,0 +1,53 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+
+$(function(){
+    
+    $('#alertMe').click(function(e){
+        
+        e.preventDefault();
+        
+        $('#successAlert').slideToggle();
+        
+    });
+    
+    $('a.pop').click(function(e){
+        e.preventDefault();
+    });
+    
+    $('a.pop').popover();
+    
+    $('[rel="tooltip"]').tooltip();
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
